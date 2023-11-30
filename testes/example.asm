c1f8a49b745caafc4fb9dd8620202afde4d26236
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-30 17:04:54 -0500

    implement the compat directive into all files

--- testes/example.asm
+++ testes/example.asm
@@ -1,6 +1,11 @@
 # Copyright 2019-2023 Kai D. Gonzalez
 # more code here ...
 
+; compat directive allows you to enforce which bytecode format is used
+; this directive will error if the format is not switched
+; this directive will also error if the format is not supported
+[compat nexfuse]
+
 @8:
   echo 0x47
 
