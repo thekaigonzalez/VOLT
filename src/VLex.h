7e944972fdadf5fff1fc0a56313ccd661f382ef8
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-30 17:04:17 -0500

    add compiler directive tokens

--- src/VLex.h
+++ src/VLex.h
@@ -16,6 +16,8 @@
 #define VTOKEN_SUBROUTINE_DEF_LEGACY '@' // @header:
 #define VTOKEN_PARAM_SEPARATOR ','       // ,
 #define VTOKEN_PARAM_DEF(x) isspace (x)  // parameter
+#define VTOKEN_DIRECTIVE_BEGIN '['       // [directive <param>]
+#define VTOKEN_DIRECTIVE_END ']'         // ]
 
 typedef enum VLexerState VLexerState;
 
