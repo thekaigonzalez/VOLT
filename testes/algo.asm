30f6940b4d8dc77bbf683dbdca9fee9fdaa830fb
Kai D. Gonzalez <gkai70263@gmail.com>
2023-12-01 16:17:25 -0500

    fix algorithm example

--- testes/algo.asm
+++ testes/algo.asm
@@ -7,7 +7,7 @@ a:
 ; increment R1, this will go up until it's equal to 'A'
   inc R1
 ; jump back to comparisons
-  jmp c
+  jmp d
 
 ; it'll just print 'DONE'
 b:
@@ -17,7 +17,10 @@ b:
 c:
 ; we're going to compare R1 and R2, if R1 is 'A', we'll jump to b, otherwise
 ; we'll go to `a' and see what we can do from there
-  cmp R1,R2,a,b
+  cmp R1,R2,b,a
+
+d:
+  jmp c
 
 m:
 ; dest reg
