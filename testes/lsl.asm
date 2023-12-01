85cb4bb4d49814d009c5be610f36837ffce2a3dd
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-30 20:24:39 -0500

    add new 'in' command

--- testes/lsl.asm
+++ testes/lsl.asm
@@ -1,5 +1,7 @@
 ; $Id: lsl.asm
 
+[compat nexfuse] ; `lsl' is not supported in OpenLUD
+
 m:
 ; lsl can consecutively add bytes to a register
   lsl R1,'H','e','l','l','o',0x0a
@@ -8,3 +10,9 @@ m:
 ; lsl can add bytes to a register
   lsl R2,'W','o','r','l','d',0x0a
   each R2
+
+; `in' can get input from the keyboard
+; depending on which VM you are using, this function may vary,
+; in NexFUSE, `in' is a binding to fgetc(stdin)
+  in R12
+  each R12
