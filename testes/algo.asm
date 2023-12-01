d43ce530ee41d012c909629ccf1476430c8b4431
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-30 20:55:03 -0500

    add simple algorithm to test adding register

---
+++ testes/algo.asm
@@ -0,0 +1,26 @@
+; $Id: algo.asm
+
+; simple algorithm
+[compat nexfuse]
+
+a:
+; increment R1, this will go up until it's equal to 'A'
+  inc R1
+; jump back to comparisons
+  jmp c
+
+; it'll just print 'DONE'
+b:
+  lsl R3,'D','O','N','E',0x0a
+  each R3
+
+c:
+; we're going to compare R1 and R2, if R1 is 'A', we'll jump to b, otherwise
+; we'll go to `a' and see what we can do from there
+  cmp R1,R2,a,b
+
+m:
+; dest reg
+  mov R2,'A'
+; jmp to compare sub
+  jmp c
