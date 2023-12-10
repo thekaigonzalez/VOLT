f1bc4ca6927c4cf7b65d9e5756c07f41ea5f1f00
Kai D. Gonzalez <gkai70263@gmail.com>
2023-12-10 11:59:03 -0500

    add new error with stack trace function

--- src/VObject.c
+++ src/VObject.c
@@ -1,6 +1,8 @@
 #include "VObject.h"
 #include "VDefaults.h"
+#include "VMemoryPool.h"
 
+#include <stdarg.h>
 #include <stdio.h>
 
 struct VObject
@@ -38,6 +40,52 @@ v_objectPool (VObject *object)
     {
       return object->pool;
     }
+
+  return NULL;
+}
+int
+v_errorWithStackTrace (VObject *object, const char *from_function,
+                       const char *message, ...)
+{
+  if (object)
+    {
+      va_list args;
+      va_start (args, message);
+
+      printf ("  Compiler Error (Called from %s)\n", from_function);
+      printf ("  Message          | ");
+      fflush (stdout);
+      vfprintf (stderr, message, args);
+      fflush (stderr);
+      printf ("\n");
+      printf ("  Memory Pool Cap. | %d\n",
+              v_memoryPoolCapacity (object->pool));
+      printf ("  Memory Pool Size | %d\n", v_memoryPoolSize (object->pool));
+
+      printf ("Stack Trace:\n");
+      printf ("  ==============================\n");
+
+      for (int i = 0; i < v_memoryPoolSize (object->pool); i++)
+        {
+          if (!v_memoryPoolAt (object->pool, i))
+            {
+              printf ("    %d | (nonexistent)\n", i);
+            }
+          else
+            {
+              printf ("    instance %d asked for heap size of `%d' (%p)\n", i,
+                      v_memoryPoolSizeof (object->pool, i),
+                      v_memoryPoolAt (object->pool, i));
+            }
+        }
+
+      va_end (args);
+
+      v_deleteMemoryPool (object->pool);
+
+      exit (EXIT_FAILURE);
+    }
+  exit (EXIT_FAILURE);
 }
 
 void
