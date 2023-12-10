9c83e36e87bf8d29291ecdb4f458fba493a67f8b
Kai D. Gonzalez <gkai70263@gmail.com>
2023-12-10 11:59:11 -0500

    add function and macro

--- src/VObject.h
+++ src/VObject.h
@@ -10,9 +10,13 @@
 // responsible for clearing it's own pool.
 typedef struct VObject VObject;
 
+#define v_error(object, message, ...)                                         \
+  v_errorWithStackTrace (object, __func__, message, ##__VA_ARGS__)
+
 VObject *v_newObject ();
 VMemoryPool *v_objectPool (VObject *object);
-
+int v_errorWithStackTrace (VObject *object, const char *from_function,
+                           const char *message, ...);
 void v_deleteObject (VObject *object);
 
 #endif // POBJECT_H
